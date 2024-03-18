0e81c36e91488a92db5b08301452f550bd5df296
Mehedi-ICE16 <mehedi.ice.16@gmail.com>
2024-03-18 13:16:48 +0600

    firstCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder ,FormGroup,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'image-show';
+  index:number = 0;
+  img_src1: string[] = ["./assets/jabed.jpg","./assets/malek.jpg","./assets/mehedi.jpg"];
+  len1:number = this.img_src1.length;
+  img_src2: string[] = ["./assets/malek.jpg","./assets/mehedi.jpg"];
+  len2:number = this.img_src2.length;
+  img_src3: string[] = ["./assets/malek.jpg"];
+  len3:number = this.img_src3.length;
+  step = 1;
+
+  constructor(private fb: FormBuilder) {}
+  applicationForm: FormGroup = this.fb.group({
+    name: this.fb.control('',Validators.required),
+    nid: this.fb.control('',Validators.required),
+    mobile: this.fb.control('',Validators.required),
+    email: this.fb.control('',Validators.required),
+    father_name: this.fb.control('',Validators.required),
+    mother_name: this.fb.control('',Validators.required),
+  })
+
+  nextStepControl() {
+    this.step++;
+    this.index=0;
+  }
+  onSubmit(){
+    this.step++;
+  }
+
+  // onSelectFile(e:any){
+  //   if(e.target.files){
+  //     const reader = new FileReader();
+  //     reader.readAsDataURL(e.target.files[0]);
+  //     reader.onload = (event:any)=>{
+  //       this.img_src.push(event.target.result);
+  //     }
+  //   }
+  // }
+
+  itemControl(i:number){
+    this.index = i;
+  }
+
 }
